6452778248dfe6c5af055761553bae0b76e0a6b4
sevillabetis <cuenta2222@gmail.com>
2014-08-01 14:43:48 +0200

    movimiento slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+var pos = 0;
+var intv;
 
+$(document).on('ready',function(){
+	init();
+});
+
+function init(){
+	$('.slider_controls li').on('click',handleClick);
+	var width = $('.slider_container').width();
+	$('.slide').each(function(i,e){
+		var url = $(e).data('background');
+		$(e).css('width',width+'px');
+		$(e).css('background-image',"url("+(url+".jpg")+")");
+	});
+	intv = setInterval(handleClick,3000);
+}
+
+function handleClick(){
+	var slide_target = 0;
+	if ($(this).parent().hasClass('slider_controls')) {
+		slide_target = $(this).index();
+		pos = slide_target;
+		clearInterval(intv);
+		intv = setInterval(handleClick,3000);
+		
+	}
+	else {
+		pos++;
+		if(pos>=$('.slide').length){
+			pos = 0;
+		}
+		slide_target = pos;
+	}
+
+	/*$('.slideContainer').animate({
+			'margin-left': -(slide_target * $('.slider_container').width()) + 'px'
+			},'slow');*/
+
+	$('.slideContainer').fadeOut('slow', function() {
+		$(this).animate({
+			'margin-left': -(slide_target * $('.slider_container').width()) + 'px'
+			},'slow', function() {
+			$(this).fadeIn();
+		});
+		
+	});
+
+}
